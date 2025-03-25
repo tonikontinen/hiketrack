538a1091c5e71ffd5f6d421d9c77f970a47fc9a4
Toni K <kontiton@gmail.com>
2025-03-25 15:39:14 +0200

    Updated HTML, CSS and JS files

--- script.js
+++ script.js
@@ -21,14 +21,55 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--distance");
 const inputRating = document.querySelector(".form__input--rating");
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // handling clicks on map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
     },
     function () {
       alert("Could not get your position");
     }
   );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value = inputDuration.value = inputRating.value = "";
+  // Display marker
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxwidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        classname: "hiking-popup",
+      })
+    )
+    .setPopupContent("Hiking")
+    .openPopup();
+});
